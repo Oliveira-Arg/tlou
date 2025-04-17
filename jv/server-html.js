93d772a28069cc9aafda80f9676d5c205a201a95
joao <oliveira.arg22@gmail.com>
2025-04-17 18:05:17 -0400

    add pages

--- jv/server-html.js
+++ jv/server-html.js
@@ -1,43 +1,47 @@
-/*
-Servidor HTTP que irá responder com código HTML
-*/
+/**
+ * Servidor HTTP contento rotas para várias páginas html
+ * http://127.0.0.1:3000/
+ */
 
-// importar o módulo http
+// importtação dos pacotes
 const http = require('http');
+const fs = require('fs'); // módulo para acessar os arquivos
 
 // Definir as configurações do servidor
 const hostname = '127.0.0.1';
 port = 3000;
 
-// Criar o servidor
+// Cria o servidor
 const server = http.createServer((req, res) => {
   // define as configuração do cabeçalho da resposta
   res.statusCode = 200; // indica sucesso
   // define o tipo de resposta
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8'); // html
 
-  // define o código html
-  const codigoHtml = `
-    <h1>Servidor HTTP rodando com Node.js</h1>
+  // obter a url
+  const url = req.url;
 
-    <p>Este é um servidor HTTP que foi criado com o módulo http do Node.js</p>
-    <p>O servidor está rodando na porta 3000.</p>
+  // variável para armazenar a página html
+  let arquivoHtml;
 
-    <p>Lista de compras</p>
-    <ul>
-      <li>arroz</li>
-      <li>feijão</li>
-      <li>óleo</li>
-      <li>macarrão</li>
-      <li>cebola</li>
-    </ul>
-  `;
+  // verificar qua a rota informada pelo usuário
+  if (url == '/') {
+    // carregar o arquivo html
+    arquivoHtml = fs.readFileSync('./public/index.html');
+  } else if (url == '/sobre') {
+    // carregar o arquivo html
+    arquivoHtml = fs.readFileSync('./public/sobre.html');
+  } else if (url == '/contato') {
+    // carregar o arquivo html
+    arquivoHtml = fs.readFileSync('./public/contato.html');
+  } else {
+    // carregar o arquivo html
+    arquivoHtml = fs.readFileSync('./public/erro.html');
+  }
 
-  // adiciona o código html
-  res.write(codigoHtml);
+  // enviar o conteúdo que está no arquivo html
+  res.end(arquivoHtml);
 
-  // enviar o conteúdo
-  res.end();
 });
 
 // iniciar o servidor
